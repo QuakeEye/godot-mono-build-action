f683f5e540d4210444cb9b6cac654752cb285ae3
Quake <QuakeEye@users.noreply.GitHub.com>
2024-01-18 08:42:40 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM barichello/godot-ci:mono-latest
+
+# Install dotnet sdk
+RUN apt-get update
+RUN apt-get install -y wget
+RUN wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+RUN chmod +x ./dotnet-install.sh
+RUN ./dotnet-install.sh --version latest
+
+# Add dotnet to path
+ENV PATH="/root/.dotnet:${PATH}"
+
+LABEL "com.github.actions.name"="Build Godot Mono"
+LABEL "com.github.actions.description"="Build a Godot mono project"
+
+LABEL repository="https://github.com/QuakeEye/godot-mono-build-action"
+
+USER root
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+set -e
+
+
+# Move godot templates already installed from the docker image to home
+mkdir -v -p ~/.local/share/godot/export_templates
+cp -a /root/.local/share/godot/templates/. ~/.local/share/godot/export_templates/
+
+
+# Verify the dotnet installation
+echo "Installed Dotnet SDK version:"
+dotnet --version
+echo "Installed Mono version:"
+mono --version
+
+
+# Set the subdirectory location, if provided
+if [ "$3" != "" ]
+then
+    SubDirectoryLocation="$3/"
+fi
+
+
+# Set the export mode, based on the debug flag parameter
+mode="export-release"
+if [ "$6" = "true" ]
+then
+    echo "Exporting in debug mode!"
+    mode="export-debug"
+fi
+
+
+# Export
+echo "Building $1 for $2"
+mkdir -p $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}
+cd "$GITHUB_WORKSPACE/$5"
+godot --headless --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
+echo "Build Done"
+
+echo ::set-output name=build::build/${SubDirectoryLocation:-""}
+
+
+# Pack the build, if requested
+if [ "$4" = "true" ]
+then
+    echo "Packing Build"
+    mkdir -p $GITHUB_WORKSPACE/package
+    cd $GITHUB_WORKSPACE/build
+    zip $GITHUB_WORKSPACE/package/artifact.zip ${SubDirectoryLocation:-"."} -r
+    echo ::set-output name=artifact::package/artifact.zip
+    echo "Done"
+fi
