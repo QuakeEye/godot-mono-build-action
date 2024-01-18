39a8e0cd79abedee76f9b9d6ae48e63e331aaa5f
Quake <QuakeEye@users.noreply.GitHub.com>
2024-01-18 15:13:17 +0100

    feat: Add semantion versioning to the project

--- release.config.js
+++ release.config.js
@@ -4,11 +4,18 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     [
-      "@semantic-release/exec",
-      {
-        "prepareCmd": "set-version ${nextRelease.version}",
-        "publishCmd": "publish-package"
-      }
+      "@semantic-release/exec"
+      
+      
+      // Theoretical scenario for possible extension in which the project
+      //  has its version set and is published respectively with the following
+      //  commands:
+      
+      //,
+      // {
+      //   "prepareCmd": "set-version ${nextRelease.version}",
+      //   "publishCmd": "publish-package"
+      // }
     ]
   ],
   "branches": [
