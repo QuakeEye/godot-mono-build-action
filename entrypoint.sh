63325344079cb0524c19ba36c5e222787070eb03
Quake <QuakeEye@users.noreply.GitHub.com>
2024-01-18 20:13:59 +0100

    test: Try to test outputting the godot output as a log

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,6 +34,7 @@ fi
 echo "Building $1 for $2"
 mkdir -p $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}
 cd "$GITHUB_WORKSPACE/$5"
+mkdir /home/runner/work/godot-mono-build-action/godot-mono-build-action/
 godot --headless --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1 > /home/runner/work/godot-mono-build-action/godot-mono-build-action/godotheadless.log
 
 # Check the exit code of the last command
