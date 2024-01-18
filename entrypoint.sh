672eb4af8769c2b236b8c8aac423936222092a50
Quake <QuakeEye@users.noreply.GitHub.com>
2024-01-18 20:32:45 +0100

    test: Test for error messages by grepping the godot log and stderr stream

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,12 +36,25 @@ mkdir -p $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}
 cd "$GITHUB_WORKSPACE/$5"
 godot --headless --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1 > $GITHUB_WORKSPACE/godotheadless.log 2>&1
 
-# Check the exit code of the last command
-#  This does not yet work, as godot always returns 0: https://github.com/godotengine/godot/issues/83042
+
+# Check the exit code of the last command using exit code
+
+# This method does not yet work, as godot always returns 0: https://github.com/godotengine/godot/issues/83042
+# It would be more elegant than the current solution, so I'm leaving it here for now.
+
 # if [ $? -ne 0 ]; then
 #   echo "Godot build failed. Exiting with error."
 #   exit 1
 # fi
+
+
+# Check if the build was successful by grepping the log file for a possible error message
+grep -q "ERROR" $GITHUB_WORKSPACE/godotheadless.log
+if [ $? -ne 0 ]; then
+  echo "Godot build failed. Exiting with error."
+  exit 1
+fi
+
 
 echo "Build Done"
 
