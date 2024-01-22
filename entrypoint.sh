0b9a63125acec0bc39962b38375ae4d302c5f4c3
Quake <QuakeEye@users.noreply.GitHub.com>
2024-01-22 14:08:38 +0100

    fix: Redefine error testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,37 @@
 set -e
 
 
+# Function definitions
+check_for_errors() {
+
+    # Check the exit code of the last command
+
+    # This method does not yet work, as godot always returns 0: https://github.com/godotengine/godot/issues/83042
+    # It would be more elegant than the current solution, so I'm leaving it here for now.
+
+    # if [ $? -ne 0 ]; then
+    #   echo "Godot build failed. Exiting with error. This is the build log:"
+    #   cat godot_error.log
+    #   exit 1
+    # fi
+
+
+    # A hacky way to do this but as mentioned Godot currently always returns 0
+    errors=grep -c "Error: " godot_error.log
+    ignoredErrors=grep -c "!EditorSettings::get_singleton() || !EditorSettings::get_singleton()->has_setting(p_setting)" godot_error.log
+
+    echo "Found $errors errors in the project. Ignoring $ignoredErrors errors."
+
+    # Check if the project is valid
+    if [ $errors -gt 0 ] && [ $errors -gt $ignoredErrors ]
+    then
+        echo "Godot project is invalid/build has failed. Exiting with error. This is the build log:"
+        cat godot_error.log
+        exit 1
+    fi
+}
+
+
 # Move godot templates already installed from the docker image to home
 mkdir -v -p ~/.local/share/godot/export_templates
 cp -a /root/.local/share/godot/templates/. ~/.local/share/godot/export_templates/
@@ -38,44 +69,20 @@ mkdir -p $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}
 cd "$GITHUB_WORKSPACE/$5"
 
 # Set up files required for the build
-echo "Setting up files required for the build"
-ls -la .
+echo "Setting up editor files required for the build"
 godot --headless --editor --quit-after 3000 . 2> godot_error.log
 
-# Check if the project is valid
-# if grep -q "ERROR: " godot_error.log
-# then
-#     echo "Godot project is invalid. Exiting with error. This is the project folder population log:"
-#     cat godot_error.log
-#     exit 1
-# fi
+check_for_errors
 
-ls -la ./.godot
 
 # Build the project
 echo "Building the project"
-ls -la .
 godot --headless --verbose --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1 2> godot_error.log
 
 
-# Check the exit code of the last command using exit code
-
-# This method does not yet work, as godot always returns 0: https://github.com/godotengine/godot/issues/83042
-# It would be more elegant than the current solution, so I'm leaving it here for now.
-
-# if [ $? -ne 0 ]; then
-#   echo "Godot build failed. Exiting with error."
-#   exit 1
-# fi
-
-
 # Check if the build was successful by grepping the log file for a possible error message
-# if grep -q "ERROR: " godot_error.log
-# then
-#     echo "Godot build failed. Exiting with error. This is the build log:"
-#     cat godot_error.log
-#     exit 1
-# fi
+check_for_errors
+
 
 echo "Build Done"
 
